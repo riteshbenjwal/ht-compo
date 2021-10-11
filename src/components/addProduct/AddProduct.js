ab453ee5a0e9310743126f76932890981d892723
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-10-11 10:39:53 +0530

    COMMENTS_ADDED

--- src/components/addProduct/AddProduct.js
+++ src/components/addProduct/AddProduct.js
@@ -11,7 +11,6 @@ import {
   Button,
 } from "react-bootstrap";
 
-import Productimg from "./productimg.jpg";
 
 const AddProduct = () => {
   return (
@@ -20,7 +19,7 @@ const AddProduct = () => {
         <h1 className="title my-4">Add Products</h1>
 
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Group className="mb-2" controlId="formBasicName">
             <Form.Label>Product Name</Form.Label>
             <Form.Control
               className="input-field"
@@ -29,7 +28,7 @@ const AddProduct = () => {
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicBrand">
+          <Form.Group className="mb-2" controlId="formBasicBrand">
             <Form.Label>Product Brand</Form.Label>
             <Form.Control
               className="input-field"
@@ -43,15 +42,28 @@ const AddProduct = () => {
             <Form.Control className="input-field" type="file" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Product Description</Form.Label>
             <Form.Control className="input-field" as="textarea" rows={3} />
           </Form.Group>
         </Form>
-        <Button variant="warning">Warning</Button>
+
+        <Button variant="warning">Add Product</Button>
       </Container>
     </>
   );
 };
 
 export default AddProduct;
+
+
+/* 
+
+Sample Add Products
+
+1. We can again make a sub Form Component and pass className, type as Props to make code look small.
+
+2. Color, Fonts, Font-size, Width and other UI things can be changed according to the Place where we have to put this component.
+
+
+*/
